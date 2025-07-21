277957f01218423a05cd6df7a8a38b7178ae681e
Ivan Korneychuk <ikorneyc@uwaterloo.ca>
2025-07-20 21:13:41 -0400

    improve footer

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -4,10 +4,10 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="relative overflow-x-clip">
+    <footer className="relative overflow-x-clip pt-8">
       <div className="absolute -z-10 h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]"></div>
       <div className="container relative z-10">
-        <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
           <p className="text-white/40">
             &copy; 2025 | Created By Ivan Korneychuk
           </p>
